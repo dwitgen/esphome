3fbad681a217d4292b0b863097a3ead4c8e9b0c5
dwit <141568444+dwitgen@users.noreply.github.com>
2024-06-12 13:26:43 -0400

    Update esp_adf_speaker.cpp adjust btn adc thresholds

--- components/esp_adf/speaker/esp_adf_speaker.cpp
+++ components/esp_adf/speaker/esp_adf_speaker.cpp
@@ -32,10 +32,10 @@ static const char *const TAG = "esp_adf.speaker";
 #define ADC_CHANNEL      ADC1_CHANNEL_7  // GPIO 8
 
 // Define thresholds for button detection in ADC values
-#define VOL_UP_THRESHOLD_LOW     807
-#define VOL_UP_THRESHOLD_HIGH    1142
-#define VOL_DOWN_THRESHOLD_LOW   1266
-#define VOL_DOWN_THRESHOLD_HIGH  1650
+#define VOL_UP_THRESHOLD_LOW     420
+#define VOL_UP_THRESHOLD_HIGH    520
+#define VOL_DOWN_THRESHOLD_LOW   950
+#define VOL_DOWN_THRESHOLD_HIGH  1000
 
 void ESPADFSpeaker::set_volume(int volume) {
     ESP_LOGI(TAG, "Setting volume to %d", volume);
