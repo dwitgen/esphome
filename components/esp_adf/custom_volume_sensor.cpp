161517a464d7089214df9fc5fb250718d95f1685
dwit <141568444+dwitgen@users.noreply.github.com>
2024-06-14 18:22:14 -0400

    Update custom_volume_sensor.cpp incorrect code initially applied

--- components/esp_adf/custom_volume_sensor.cpp
+++ components/esp_adf/custom_volume_sensor.cpp
@@ -1,21 +1,17 @@
-#pragma once
-
-#include "esphome.h"
+#include "custom_volume_sensor.h"
 
 namespace esphome {
 namespace custom_components {
 
-class CustomVolumeSensor : public sensor::Sensor, public Component {
- public:
-  void setup() override {
-    ESP_LOGCONFIG(TAG, "Setting up Custom Volume Sensor...");
-  }
+void CustomVolumeSensor::setup() {
+  ESP_LOGCONFIG("custom_volume_sensor", "Setting up Custom Volume Sensor...");
+}
 
-  void update_volume(int volume) {
-    ESP_LOGI(TAG, "Updating volume to %d", volume);
-    this->publish_state(volume);
-  }
-};
+void CustomVolumeSensor::update_volume(int volume) {
+  ESP_LOGI("custom_volume_sensor", "Updating volume to %d", volume);
+  this->publish_state(volume);
+}
 
 }  // namespace custom_components
 }  // namespace esphome
+
