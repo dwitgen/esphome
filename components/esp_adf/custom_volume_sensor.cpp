1adada3056618f31e79e1809d5c6c1cfcb75e5bf
dwit <141568444+dwitgen@users.noreply.github.com>
2024-06-14 18:49:30 -0400

    Update custom_volume_sensor.cpp remove code again

--- components/esp_adf/custom_volume_sensor.cpp
+++ components/esp_adf/custom_volume_sensor.cpp
@@ -3,14 +3,14 @@
 namespace esphome {
 namespace custom_components {
 
-void CustomVolumeSensor::setup() {
-  ESP_LOGCONFIG("custom_volume_sensor", "Setting up Custom Volume Sensor...");
-}
+//void CustomVolumeSensor::setup() {
+//  ESP_LOGCONFIG("custom_volume_sensor", "Setting up Custom Volume Sensor...");
+//}
 
-void CustomVolumeSensor::update_volume(int volume) {
-ESP_LOGI("custom_volume_sensor", "Updating volume to %d", volume);
-  this->publish_state(volume);
-}
+//void CustomVolumeSensor::update_volume(int volume) {
+//ESP_LOGI("custom_volume_sensor", "Updating volume to %d", volume);
+//  this->publish_state(volume);
+//}
 
 }  // namespace custom_components
 }  // namespace esphome
