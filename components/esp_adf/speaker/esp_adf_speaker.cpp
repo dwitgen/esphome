c7f6d277c6e3e39745ffd84912c16bb1ca0b6b99
dwit <141568444+dwitgen@users.noreply.github.com>
2024-07-09 12:04:33 -0400

    Update esp_adf_speaker.cpp added functions to handle buttons

--- components/esp_adf/speaker/esp_adf_speaker.cpp
+++ components/esp_adf/speaker/esp_adf_speaker.cpp
@@ -198,6 +198,102 @@ void ESPADFSpeaker::setup() {
    
 }
 
+// Added key input and button handling
+esp_err_t ESPADFSpeaker::input_key_service_cb(periph_service_handle_t handle, periph_service_event_t *evt, void *ctx) {
+    ESPADFSpeaker *instance = static_cast<ESPADFSpeaker*>(ctx);
+    int32_t id = static_cast<int32_t>(reinterpret_cast<uintptr_t>(evt->data));
+
+    // Read the ADC value
+    int adc_value = adc1_get_raw(ADC1_CHANNEL_3);  // Replace with your ADC channel
+    ESP_LOGI(TAG, "Button event callback received: id=%d, event type=%d, ADC value=%d", id, evt->type, adc_value);
+
+    instance->handle_button_event(id, evt->type);
+    return ESP_OK;
+}
+
+void ESPADFSpeaker::handle_button_event(int32_t id, int32_t event_type) {
+    ESP_LOGI(TAG, "Handle Button event received: id=%d", id);
+    if (event_type != 1 && event_type != 3) { // Only process the event if the event_type is 1 click action or 3 long press action
+        ESP_LOGI(TAG, "Ignoring event with type: %d", event_type);
+        return;
+    }
+    uint32_t current_time = millis();
+    static uint32_t last_button_press[7] = {0};
+    uint32_t debounce_time = 200;
+
+    if (id == BUTTON_MODE_ID) {
+        debounce_time = 500;
+    }
+
+    if (current_time - last_button_press[id] > debounce_time) {
+        switch (id) {
+            case 0:
+                ESP_LOGI(TAG, "Unkonw Button detected");
+                //volume_down();
+                break;
+            case 1:
+                ESP_LOGI(TAG, "Record button detected");
+                handle_rec_button();
+                break;
+            case 2:
+                ESP_LOGI(TAG, "Set button detected");
+                handle_set_button();
+                break;
+            case 3:
+                ESP_LOGI(TAG, "Play button detected");
+                handle_play_button();
+                break;
+            case 4:
+                ESP_LOGI(TAG, "Mode button detected");
+                handle_mode_button();
+                break;
+            case 5:
+                ESP_LOGI(TAG, "Volume down detected");
+                volume_down();
+                break;
+            case 6:
+                ESP_LOGI(TAG, "Volume up detected");
+                volume_up();
+                break;
+            default:
+                ESP_LOGW(TAG, "Unhandled button event id: %d", id);
+                break;
+        }
+        last_button_press[id] = current_time;
+    }
+}
+
+
+
+void ESPADFSpeaker::handle_mode_button() {
+    ESP_LOGI(TAG, "Play mode action");
+   // if (this->state_ != speaker::STATE_RUNNING && this->state_ != speaker::STATE_STARTING) {
+   //     ESP_LOGI(TAG, "Mode button, speaker stopped");
+   //     this->play_url("http://streaming.tdiradio.com:8000/house.mp3");
+   // } else {
+   //     ESP_LOGI(TAG, "State is stopping");
+   //     this->cleanup_audio_pipeline();
+   //     this->stop();
+   // }
+}
+
+void ESPADFSpeaker::handle_play_button() {
+    ESP_LOGI(TAG, "Play button action");
+    // Add code to play
+}
+
+void ESPADFSpeaker::handle_set_button() {
+    ESP_LOGI(TAG, "Set button action");
+    // Add code to handle set action
+}
+
+void ESPADFSpeaker::handle_rec_button() {
+    ESP_LOGI(TAG, "Record button action");
+    // Add code to start recording
+}
+
+// End button handlinh
+
 void ESPADFSpeaker::start() { this->state_ = speaker::STATE_STARTING; }
 void ESPADFSpeaker::start_() {
   if (!this->parent_->try_lock()) {
