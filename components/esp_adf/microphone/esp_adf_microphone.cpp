ffeece98d8dd254951ed2a30c75765e7c9ae8f16
dwit <141568444+dwitgen@users.noreply.github.com>
2024-07-10 11:33:09 -0400

    Update esp_adf_microphone.cpp

--- components/esp_adf/microphone/esp_adf_microphone.cpp
+++ components/esp_adf/microphone/esp_adf_microphone.cpp
@@ -107,6 +107,12 @@ void ESPADFMicrophone::read_task(void *params) {
       .fixed_mclk = 0,
       .mclk_multiple = I2S_MCLK_MULTIPLE_256,
       .bits_per_chan = I2S_BITS_PER_CHAN_DEFAULT,
+      .chan_mask = I2S_CHANNEL_MONO,    // Correct value type, adjust as necessary
+      .total_chan = 2,                  // Default value, adjust as necessary
+      .left_align = false,              // Default value
+      .big_edin = false,                // Default value
+      .bit_order_msb = true,            // Default value
+      .skip_msk = false,                 // Default value
       /*.chan_mask = NULL,
       .total_chan = NULL,
       .left_align = FALSE,
