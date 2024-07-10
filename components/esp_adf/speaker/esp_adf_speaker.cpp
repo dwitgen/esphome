125efd68730cdbaa7f41409cc85a3e14fc1a1672
dwit <141568444+dwitgen@users.noreply.github.com>
2024-07-10 10:53:41 -0400

    Update esp_adf_speaker.cpp

--- components/esp_adf/speaker/esp_adf_speaker.cpp
+++ components/esp_adf/speaker/esp_adf_speaker.cpp
@@ -182,8 +182,8 @@ void ESPADFSpeaker::player_task(void *params) {
       .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
       .communication_format = I2S_COMM_FORMAT_STAND_I2S,
       .intr_alloc_flags = ESP_INTR_FLAG_LEVEL2 | ESP_INTR_FLAG_IRAM,
-      .dma_buf_count = 8,
-      .dma_buf_len = 1024,
+      .dma_buf_count = 16,  // Increased from 8
+      .dma_buf_len = 2048,  // Increased from 1024
       .use_apll = false,
       .tx_desc_auto_clear = true,
       .fixed_mclk = 0,
